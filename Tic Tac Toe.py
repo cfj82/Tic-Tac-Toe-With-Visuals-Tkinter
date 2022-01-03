32e4d21b8e5a1ef2e00b31b1776566841d674fc7
Colin Fowler <92275816+cfj82@users.noreply.github.com>
2022-01-02 20:58:57 -0800

    bug corrections

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -9,12 +9,15 @@ root.configure(width=300, height=250)
 
 count = 0
 
+    # todo AI auto move
+
 def b1():
     global count
     if b1["text"] == "":
         b1["text"] = change()
         count = count + 1
         check()
+
 
 def b2():
     global count
@@ -23,12 +26,14 @@ def b2():
         count = count + 1
         check()
 
+
 def b3():
     global count
     if b3["text"] == "":
         b3["text"] = change()
         count = count + 1
         check()
+
 
 def b4():
     global count
@@ -37,12 +42,14 @@ def b4():
         count = count + 1
         check()
 
+
 def b5():
     global count
     if b5["text"] == "":
         b5["text"] = change()
         count = count + 1
         check()
+
 
 def b6():
     global count
@@ -51,12 +58,14 @@ def b6():
         count = count + 1
         check()
 
+
 def b7():
     global count
     if b7["text"] == "":
         b7["text"] = change()
         count = count + 1
         check()
+
 
 def b8():
     global count
@@ -65,12 +74,14 @@ def b8():
         count = count + 1
         check()
 
+
 def b9():
     global count
     if b9["text"] == "":
         b9["text"] = change()
         count = count + 1
         check()
+
 
 def change():
     global count
@@ -81,6 +92,7 @@ def change():
         output.set("Player 2 Turn")
         return "O"
 
+
 def quit():
     msg = messagebox.askyesno("Quit", "Do You Want To Quit?")
     if msg == True:
@@ -90,76 +102,76 @@ def quit():
 def check():
     global count
     if (b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X" or  # win top row
-        b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X" or  # win middle row
-        b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X" or  # win bottom row
-        b7["text"] == "X" and b4["text"] == "X" and b1["text"] == "X" or  # win left column
-        b8["text"] == "X" and b5["text"] == "X" and b2["text"] == "X" or  # win middle column
-        b9["text"] == "X" and b6["text"] == "X" and b3["text"] == "X" or  # win right column
-        b7["text"] == "X" and b5["text"] == "X" and b3["text"] == "X" or  # win diagonal
-        b9["text"] == "X" and b5["text"] == "X" and b1["text"] == "X"):  # win other diagonal
+            b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X" or  # win middle row
+            b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X" or  # win bottom row
+            b7["text"] == "X" and b4["text"] == "X" and b1["text"] == "X" or  # win left column
+            b8["text"] == "X" and b5["text"] == "X" and b2["text"] == "X" or  # win middle column
+            b9["text"] == "X" and b6["text"] == "X" and b3["text"] == "X" or  # win right column
+            b7["text"] == "X" and b5["text"] == "X" and b3["text"] == "X" or  # win diagonal
+            b9["text"] == "X" and b5["text"] == "X" and b1["text"] == "X"):  # win other diagonal
         output.set("Player 1 Win!")
         quit()
 
-    elif (b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O" or  #win top row
-        b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O" or  # win middle row
-        b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O" or  # win bottom row
-        b7["text"] == "O" and b4["text"] == "O" and b1["text"] == "O" or  # win left column
-        b8["text"] == "O" and b5["text"] == "O" and b2["text"] == "O" or  # win middle column
-        b9["text"] == "O" and b6["text"] == "O" and b3["text"] == "O" or  # win right column
-        b7["text"] == "O" and b5["text"] == "O" and b3["text"] == "O" or  # win diagonal
-        b9["text"] == "O" and b5["text"] == "O" and b1["text"] == "O"):  # win other diagonal
+    elif (b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O" or  # win top row
+          b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O" or  # win middle row
+          b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O" or  # win bottom row
+          b7["text"] == "O" and b4["text"] == "O" and b1["text"] == "O" or  # win left column
+          b8["text"] == "O" and b5["text"] == "O" and b2["text"] == "O" or  # win middle column
+          b9["text"] == "O" and b6["text"] == "O" and b3["text"] == "O" or  # win right column
+          b7["text"] == "O" and b5["text"] == "O" and b3["text"] == "O" or  # win diagonal
+          b9["text"] == "O" and b5["text"] == "O" and b1["text"] == "O"):  # win other diagonal
         output.set("Player 2 Win!")
         quit()
 
 
 f_board = Frame(root, bg="#000000", borderwidth=2, )
-f_board.rowconfigure([0,1,2,3], minsize=100)  # minsize for each row=100 keys
-f_board.columnconfigure([0,1,2], minsize=100)
+f_board.rowconfigure([0, 1, 2, 3], minsize=100)  # minsize for each row=100 keys
+f_board.columnconfigure([0, 1, 2], minsize=100)
 f_board.pack(fill="both", expand=True)
 
 output = StringVar()
-lbl = Label(f_board,  font=("verdana", 18), anchor="center", relief=SUNKEN, border=15,
-                 bg="#5DADE2", textvariable=output)
+lbl = Label(f_board, font=("verdana", 18), anchor="center", relief=SUNKEN, border=15,
+            bg="#5DADE2", textvariable=output)
 lbl.grid(row=0, column=0, columnspan=2, sticky="nsew")
 
 quit_btn = Button(f_board, font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 bg="#5DADE2", text="Quit", command=quit )
+                  bg="#5DADE2", text="Quit", command=quit)
 quit_btn.grid(row=0, column=2, sticky="nsew")
 
 b7 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b7)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b7)
 b7.grid(row=1, column=0, sticky="nsew")
 
 b8 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b8)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b8)
 b8.grid(row=1, column=1, sticky="nsew")
 
 b9 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b9)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b9)
 b9.grid(row=1, column=2, sticky="nsew")
 
 b4 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b4)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b4)
 b4.grid(row=2, column=0, sticky="nsew")
 
 b5 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b5)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b5)
 b5.grid(row=2, column=1, sticky="nsew")
 
 b6 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b6)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b6)
 b6.grid(row=2, column=2, sticky="nsew")
 
 b1 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b1)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b1)
 b1.grid(row=3, column=0, sticky="nsew")
 
 b2 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b2)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b2)
 b2.grid(row=3, column=1, sticky="nsew")
 
 b3 = Button(f_board, text="", font=("verdana", 18), anchor="center", relief=RAISED, border=10,
-                 activebackground="#A2D9CE", bg="#BB8FCE", command=b3)
+            activebackground="#A2D9CE", bg="#BB8FCE", command=b3)
 b3.grid(row=3, column=2, sticky="nsew")
 
 root.mainloop()
